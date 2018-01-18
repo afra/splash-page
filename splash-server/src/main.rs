262b56b24f9d2ff1c04c55adcd08736dfc4d211c
Katharina Fey <kookie@spacekookie.de>
2018-01-18 01:25:50 +0100

    Adding a first example with auth

--- splash-server/src/main.rs
+++ splash-server/src/main.rs
@@ -9,7 +9,8 @@ use rocket::Outcome;
 use rocket::http::Status;
 use rocket::request::{self, Request, FromRequest};
 
-struct ApiKey(String);
+
+struct AuthUser(String);
 
 pub fn base64_decode(data: &str) -> Option<String> {
 
@@ -18,50 +19,55 @@ pub fn base64_decode(data: &str) -> Option<String> {
 }
 
 /// Returns true if `key` is a valid API key string.
-fn is_valid(key: &str) -> bool {
+fn handle_user(key: &str) -> Option<String> {
     // TODO: Get authorised keys somehow
     // key == key[6..];
 
     // FIXME: Don't panic!
 
     if !key.starts_with("Basic ") {
-        return false;
+        return None;
     }
 
     let dec = base64_decode(&key[6..]).unwrap();
-    let split: Vec<_> = dec.split(":").collect(); 
+    let split: Vec<_> = dec.split(":").collect();
 
+    let (user, pw) = (split[0], split[1]);
 
-    return true;
+    return match (user, pw) {
+        ("afra", "1234") => Some(String::from(user)),
+        _ => None,
+    };
 }
 
-impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
+impl<'a, 'r> FromRequest<'a, 'r> for AuthUser {
     type Error = ();
 
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<ApiKey, ()> {
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<AuthUser, ()> {
         let keys: Vec<_> = request.headers().get("Authorization").collect();
         if keys.len() != 1 {
             return Outcome::Failure((Status::BadRequest, ()));
         }
 
         let key = keys[0];
-        if is_valid(keys[0]) {
-            return Outcome::Forward(())
-        }
 
-        return Outcome::Success(ApiKey(key.to_string()));
+        return match handle_user(key) {
+            Some(user) => Outcome::Success(AuthUser(user)),
+            None => Outcome::Forward(()),
+        };
     }
 }
 
 #[get("/sensitive")]
-fn sensitive(key: ApiKey) -> &'static str {
-    "Sensitive data."
+fn sensitive(user: AuthUser) -> String {
+    return format!("Hello {}", user.0);
 }
+
 #[post("/api/v1/open")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![sensitive]).launch();
 }
