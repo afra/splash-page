bd6c0cd2d214725ad9a0ee2d654674787a3a271c
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-01-20 21:01:29 +0100

    typopro

--- splash-web/ember-cli-build.js
+++ splash-web/ember-cli-build.js
@@ -3,23 +3,17 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
+  const app = new EmberApp(defaults, {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  const sourceSansPro = new Funnel('node_modules/@typopro/web-source-sans-pro', {
+    destDir: 'assets/source-sans-pro'
+  });
 
-  return app.toTree();
+	return new MergeTrees([app.toTree(), sourceSansPro]);
 };
