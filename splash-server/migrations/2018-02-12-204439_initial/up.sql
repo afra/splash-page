354393217ee33de01f42a344ec1aaefca0d29370
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-02-17 00:05:37 +0100

    fix migration on postgres

--- splash-server/migrations/2018-02-12-204439_initial/up.sql
+++ splash-server/migrations/2018-02-12-204439_initial/up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  pw_hash VARCHAR NOT NULL,
-  salt VARCHAR NOT NULL,
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR NOT NULL,
+  "pw_hash" VARCHAR NOT NULL,
+  "salt" VARCHAR NOT NULL
 );
 
 CREATE TABLE sessions (
-  id SERIAL PRIMARY KEY,
-  token VARCHAR NOT NULL,
-  user VARCHAR NOT NULL,
-  salt FOREIGN KEY (user) REFERENCES users(id),
+  "id" SERIAL PRIMARY KEY,
+  "token" VARCHAR NOT NULL,
+  "user" SERIAL NOT NULL,
+  FOREIGN KEY ("user") REFERENCES users("id")
 );
