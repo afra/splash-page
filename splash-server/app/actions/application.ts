29e4245134e35fee69ce426ad83eeb7ddc1c8680
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-02-17 01:26:55 +0100

    fix ts

--- splash-server/app/actions/application.ts
+++ splash-server/app/actions/application.ts
@@ -1,4 +1,7 @@
-import { Action } from 'denali';
+import { Action, ResponderParams } from 'denali';
 
 export default class ApplicationAction extends Action {
+  respond(params: ResponderParams) {
+    throw new Error("Method not implemented.");
+  }
 }
