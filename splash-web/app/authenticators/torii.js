f7adab7ec31a7b993ab594d464695c0b43fc5df8
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-10-06 23:55:19 +0200

    authenticate with github?

--- splash-web/app/authenticators/torii.js
+++ splash-web/app/authenticators/torii.js
@@ -1,6 +1,19 @@
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
 import { inject as service } from '@ember/service';
+import fetch from 'fetch';
 
 export default ToriiAuthenticator.extend({
   torii: service(),
+
+  async authenticate() {
+    const data = await this._super(...arguments);
+
+    const res = await fetch('/api/v1/oauth', {
+      method: 'POST',
+      data: data.authorizationCode,
+    });
+
+    const json = await res.json();
+    debugger;
+  },
 });
